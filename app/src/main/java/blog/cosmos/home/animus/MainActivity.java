5d797454cbaa9748386fad7827e97ef14d1f9f7a
s0oraj <hotmailsuraj@gmail.com>
2022-09-12 16:53:11 +0530

    test change

--- app/src/main/java/blog/cosmos/home/animus/MainActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/MainActivity.java
@@ -3,12 +3,25 @@ package blog.cosmos.home.animus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textView);
+        count =0;
+
+    }
+
+    public void onButtonClick(View view) {
+        count++;
+        text.setText("Button Clicked!\nButton has been clicked for "+count+" times.");
+
     }
 }
