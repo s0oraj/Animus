205103111636c487105a91e679554c19723197c5
s0oraj <hotmailsuraj@gmail.com>
2022-09-16 13:23:42 +0530

    Added an ic launcher and created a splash screen

---
+++ app/src/main/java/blog/cosmos/home/animus/SplashActivity.java
@@ -0,0 +1,14 @@
+package blog.cosmos.home.animus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
