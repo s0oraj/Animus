3f71ce0ce908756bd20ce7aaf90c33e5b24904f3
s0oraj <hotmailsuraj@gmail.com>
2022-09-16 10:28:53 +0530

    Changes in the following files
    CreateAccountFragment.java
    FragmentReplacerActivity.java
    activity_fragment_replacer.xml

--- app/src/main/java/blog/cosmos/home/animus/fragments/CreateAccountFragment.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/CreateAccountFragment.java
@@ -2,16 +2,34 @@ package blog.cosmos.home.animus.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import blog.cosmos.home.animus.FragmentReplacerActivity;
 import blog.cosmos.home.animus.R;
 
 
 public class CreateAccountFragment extends Fragment {
+
+    private EditText nameEt, emailEt, passwordEt, confirmPasswordEt;
+    private TextView loginTv;
+    private Button signUpBtn;
+    private FirebaseAuth auth;
+
+    public static final String EMAIL_REGEX ="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+
+
 
     public CreateAccountFragment() {
         // Required empty public constructor
@@ -23,4 +41,75 @@ public class CreateAccountFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_account, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        clickListener();
+    }
+
+
+
+
+    public void init(View view){
+
+        nameEt = view.findViewById(R.id.nameET);
+        emailEt = view.findViewById(R.id.emailET);
+        passwordEt = view.findViewById(R.id.passwordET);
+        confirmPasswordEt = view.findViewById(R.id.confirmPassET);
+        loginTv = view.findViewById(R.id.loginTV);
+        signUpBtn = view.findViewById(R.id.signUpBtn);
+
+        auth = FirebaseAuth.getInstance();
+
+
+    }
+
+    private void clickListener() {
+
+        loginTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((FragmentReplacerActivity)getActivity()).setFragment( new LoginFragment());
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = nameEt.getText().toString();
+                String email = emailEt.getText().toString();
+                String password = passwordEt.getText().toString();
+                String confirmPassword = confirmPasswordEt.getText().toString();
+
+
+                if(name.isEmpty() || name.equals(" ")){
+                    nameEt.setError("Please input valid name");
+                    return;
+
+                }
+                if(email.isEmpty() || !email.matches(EMAIL_REGEX)){
+                    nameEt.setError("Please input valid name");
+                    return;
+
+                }
+                if(name.isEmpty() || name.equals(" ")){
+                    nameEt.setError("Please input valid name");
+                    return;
+
+                }
+
+            }
+        });
+
+    }
+
+
+
+
 }
