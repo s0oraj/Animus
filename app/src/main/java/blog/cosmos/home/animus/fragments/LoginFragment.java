680a794900941b4b37e7af6a2367412e6b488e54
s0oraj <hotmailsuraj@gmail.com>
2022-09-16 22:37:58 +0530

    Some changes for sign up and login activity

--- app/src/main/java/blog/cosmos/home/animus/fragments/LoginFragment.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package blog.cosmos.home.animus.fragments;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import static blog.cosmos.home.animus.fragments.CreateAccountFragment.EMAIL_REGEX;
 
 import android.content.Intent;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +20,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import blog.cosmos.home.animus.MainActivity;
 import blog.cosmos.home.animus.R;
@@ -36,8 +44,13 @@ public class LoginFragment extends Fragment {
     private Button loginBtn, googleSignUpBtn;
     private ProgressBar progressBar;
 
-    private FirebaseAuth auth;
+    private FirebaseAuth mAuth;
 
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
 
 
 
@@ -58,7 +71,18 @@ public class LoginFragment extends Fragment {
 
         init(view);
 
-        auth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+
 
         clickListener();
 
@@ -99,14 +123,14 @@ public class LoginFragment extends Fragment {
                     return;
                 }
 
-                auth.signInWithEmailAndPassword(email,password)
+                mAuth.signInWithEmailAndPassword(email,password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
 
                                 if(task.isSuccessful()){
 
-                                    FirebaseUser user = auth.getCurrentUser();
+                                    FirebaseUser user = mAuth.getCurrentUser();
                                     if(user.isEmailVerified()){
                                         Toast.makeText(getContext(), "Please verify your email", Toast.LENGTH_SHORT).show();
                                     }
@@ -150,4 +174,45 @@ public class LoginFragment extends Fragment {
     }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        Log.d(TAG, "Got ID token.");
+
+                        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                        mAuth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Log.d(TAG, "signInWithCredential:success");
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                            updateUI(null);
+                                        }
+                                    }
+                                });
+
+                    }
+                } catch (ApiException e) {
+                    // ...
+                }
+                break;
+        }
+    }
 }
+
+
