8fd7ceb925e09c7f105b76b36dd358fde060946c
s0oraj <hotmailsuraj@gmail.com>
2022-09-16 18:39:54 +0530

    Set up the SplashActivity.java for splash screen of the app

--- app/src/main/java/blog/cosmos/home/animus/SplashActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/SplashActivity.java
@@ -2,7 +2,12 @@ package blog.cosmos.home.animus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(user==null){
+                    startActivity(new Intent(SplashActivity.this, FragmentReplacerActivity.class));
+                }else{
+
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class) );
+                }
+                finish();
+            }
+        },2500);
+
+
+
     }
 }
