e7c88cd8d47009e72de04babb66d8354f2b84ed2
s0oraj <hotmailsuraj@gmail.com>
2022-09-17 09:49:42 +0530

    working on SplashActivity.java

--- app/src/main/java/blog/cosmos/home/animus/SplashActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/SplashActivity.java
@@ -1,10 +1,13 @@
 package blog.cosmos.home.animus;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,11 +17,26 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+       // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+     /*   View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);*/
+
+
+        //setContentView(R.layout.activity_splash);
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
 
+       /* View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions); */
+
+
+      //  ActionBar actionBar = getSupportActionBar();
+      //  if (actionBar != null) { actionBar.hide(); }
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -28,11 +46,13 @@ public class SplashActivity extends AppCompatActivity {
 
                     startActivity(new Intent(SplashActivity.this, MainActivity.class) );
                 }
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
             }
-        },2500);
-
+        },1100);
 
 
     }
+
+
 }
