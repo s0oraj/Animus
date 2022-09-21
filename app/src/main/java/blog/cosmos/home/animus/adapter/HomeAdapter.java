81d2544490e921ca8bc4a7f8ac09a9dc3b70fd4a
s0oraj <hotmailsuraj@gmail.com>
2022-09-21 08:17:10 +0530

    Setting up the home fragment, home adapter and home model

---
+++ app/src/main/java/blog/cosmos/home/animus/adapter/HomeAdapter.java
@@ -0,0 +1,117 @@
+package blog.cosmos.home.animus.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+import java.util.Random;
+
+import blog.cosmos.home.animus.R;
+import blog.cosmos.home.animus.model.HomeModel;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeHolder>{
+
+    private List<HomeModel> list;
+    Context context;
+
+    public HomeAdapter(List<HomeModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public HomeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        
+        
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_items,parent,false);
+        
+        
+        return new HomeHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeHolder holder, int position) {
+        
+        holder.userNameTv.setText(list.get(position).getUserName());
+        holder.timeTv.setText(list.get(position).getTimestamp());
+
+        int count = list.get(position).getLikeCount();
+
+        if(count==0){
+            holder.likeCountTv.setVisibility(View.INVISIBLE);
+
+        }else if (count == 1){
+            holder.likeCountTv.setText(count + " like");
+
+        }else {
+            holder.likeCountTv.setText(count+ " likes");
+        }
+
+
+        Random random = new Random();
+
+        int color = Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256));
+
+        Glide.with(context.getApplicationContext())
+                .load(list.get(position).getProfileImage())
+                .placeholder(R.drawable.ic_person)
+                .timeout(6500)
+                .into(holder.profileImage);
+
+        Glide.with(context.getApplicationContext())
+                .load(list.get(position).getPostImage())
+                .placeholder(new ColorDrawable(color))
+                .timeout(7000)
+                .into(holder.imageView);
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    static class HomeHolder extends RecyclerView.ViewHolder{
+        
+        private CircleImageView profileImage;
+        private TextView userNameTv, timeTv, likeCountTv;
+        private ImageView imageView;
+        private ImageButton likeBtn, commentBtn, shareBtn;
+        
+
+        public HomeHolder(@NonNull View itemView) {
+            super(itemView);
+            
+            
+            profileImage = itemView.findViewById(R.id.profileImage);
+            imageView = itemView.findViewById(R.id.profileImage);
+            userNameTv = itemView.findViewById(R.id.nameTv);
+            timeTv = itemView.findViewById(R.id.timeTv);
+            likeCountTv = itemView.findViewById(R.id.likeCountTv);
+            likeBtn = itemView.findViewById(R.id.likeBtn);
+            commentBtn = itemView.findViewById(R.id.commentBtn);
+            shareBtn = itemView.findViewById(R.id.shareBtn);
+            
+            
+            
+        }
+    }
+    
+}
--- app/src/main/java/blog/cosmos/home/animus/fragments/Home.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/Home.java
@@ -2,21 +2,43 @@ package blog.cosmos.home.animus.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import blog.cosmos.home.animus.R;
+import blog.cosmos.home.animus.adapter.HomeAdapter;
+import blog.cosmos.home.animus.model.HomeModel;
 
 public class Home extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    HomeAdapter adapter;
+    private List<HomeModel> list;
+    private FirebaseUser user;
+
+    DocumentReference reference;
+
 
 
     public Home() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -25,4 +47,38 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        init(view);
+
+        reference = FirebaseFirestore.getInstance().collection("Posts").document(user.getUid());
+
+        list = new ArrayList<>();
+        adapter = new HomeAdapter(list, getContext());
+        recyclerView.setAdapter(adapter);
+
+        loadDataFromFireStore();
+
+    }
+
+    public void init(View view){
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+    }
+
+    private void loadDataFromFireStore(){
+
+    }
+
+
 }
