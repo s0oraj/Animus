7346065e5040dce8fa74fc0579d95e86a3826a44
s0oraj <hotmailsuraj@gmail.com>
2022-09-21 10:28:16 +0530

    Changes in following files
    fragment_profile.xml
    Profile.java

--- app/src/main/java/blog/cosmos/home/animus/fragments/Profile.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/Profile.java
@@ -2,16 +2,29 @@ package blog.cosmos.home.animus.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import blog.cosmos.home.animus.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class Profile extends Fragment {
+
+    private TextView nameTv, toolbarNameTv, statusTv, followingCountTv, followersCountTv, postCountTv;
+    private CircleImageView profileImage;
+    private Button followBtn;
+    private RecyclerView recyclerView;
+
+
 
 
     public Profile() {
@@ -26,4 +39,21 @@ public class Profile extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+
+    }
+
+    private void init(View view){
+
+
+
+    }
+
+
 }
