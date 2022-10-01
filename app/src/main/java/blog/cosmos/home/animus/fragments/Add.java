43ffa9d829f23cf5d68840d627550cb628d03076
s0oraj <hotmailsuraj@gmail.com>
2022-10-01 12:43:19 +0530

    Making changes in following files
    fragment_add.xml
    Add.java
    AndroidManifest.xml
    build.gradle
    fragment_add.xml
    ic_back.xml
    ic_next.xml
    image_items.xml
    misc.xml
    proguard-rules.pro

--- app/src/main/java/blog/cosmos/home/animus/fragments/Add.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/Add.java
@@ -3,16 +3,22 @@ package blog.cosmos.home.animus.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import blog.cosmos.home.animus.R;
 
 
 public class Add extends Fragment {
 
+    private EditText descET;
+    private ImageView imageView;
+    private RecyclerView recyclerView;
 
     public Add() {
         // Required empty public constructor
