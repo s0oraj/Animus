d3d6a097f117a65f29fb6d5f9c1c91e99f80a09a
s0oraj <hotmailsuraj@gmail.com>
2022-10-02 11:50:24 +0530

    making changes to remove dummy posts and add our own

--- app/src/main/java/blog/cosmos/home/animus/fragments/CreateAccountFragment.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/CreateAccountFragment.java
@@ -23,6 +23,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -153,6 +154,16 @@ public class CreateAccountFragment extends Fragment {
                         if(task.isSuccessful()){
 
                             FirebaseUser user = auth.getCurrentUser();
+
+                            UserProfileChangeRequest.Builder request = new UserProfileChangeRequest.Builder();
+                            request.setDisplayName(name);
+
+                            user.updateProfile(request.build());
+
+
+
+
+
                             user.sendEmailVerification()
                                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                                 @Override
