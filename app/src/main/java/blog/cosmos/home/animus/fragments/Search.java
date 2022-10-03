31aff48bf7f8916d426e4863f427cbf3e37b0b71
s0oraj <hotmailsuraj@gmail.com>
2022-10-03 07:01:42 +0530

    Initial code changes for search screen

--- app/src/main/java/blog/cosmos/home/animus/fragments/Search.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/Search.java
@@ -2,7 +2,11 @@ package blog.cosmos.home.animus.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,9 @@ import blog.cosmos.home.animus.R;
 
 public class Search extends Fragment {
 
+
+    SearchView searchView;
+    RecyclerView recyclerView;
 
     public Search() {
         // Required empty public constructor
@@ -25,4 +32,36 @@ public class Search extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+    }
+
+
+    private void init(View view){
+        searchView = view.findViewById(R.id.searchView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+    }
+
+
+
+
 }
