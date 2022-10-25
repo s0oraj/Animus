b66e63a83deb97abe9e27c464d5871dd56756f5a
s0oraj <hotmailsuraj@gmail.com>
2022-10-25 16:06:17 +0530

    Code changes for adding comment feature to posts

--- app/src/main/java/blog/cosmos/home/animus/MainActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/MainActivity.java
@@ -1,24 +1,12 @@
 package blog.cosmos.home.animus;
 
-import static android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS;
-import static blog.cosmos.home.animus.utils.Constants.PREF_DIRECTORY;
-import static blog.cosmos.home.animus.utils.Constants.PREF_NAME;
-
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -26,7 +14,6 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -37,31 +24,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
-import com.r0adkll.slidr.Slidr;
-import com.r0adkll.slidr.model.SlidrConfig;
-import com.r0adkll.slidr.model.SlidrListener;
-import com.r0adkll.slidr.model.SlidrPosition;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import blog.cosmos.home.animus.adapter.ViewPagerAdapter;
 import blog.cosmos.home.animus.fragments.Add;
 import blog.cosmos.home.animus.fragments.Comment;
-import blog.cosmos.home.animus.fragments.CreateAccountFragment;
-import blog.cosmos.home.animus.fragments.ForgotPassword;
 import blog.cosmos.home.animus.fragments.Home;
 import blog.cosmos.home.animus.fragments.MainScreenFragment;
 import blog.cosmos.home.animus.fragments.Notification;
@@ -244,10 +219,10 @@ public class MainActivity extends AppCompatActivity implements Search.OndataPass
 
                 */
 
-               // onShowPopup(popupView);
+                //onShowPopup(popupView);
 
 
-                startActivity(new Intent(MainActivity.this,ExampleActivity.class));
+               startActivity(new Intent(MainActivity.this, PopUpActivity.class));
             }
         });
     }
@@ -508,8 +483,8 @@ public class MainActivity extends AppCompatActivity implements Search.OndataPass
                                 else {
                                     // Has user scroll enough to "auto close" popup ?
                                     if ((baseLayoutPosition - currentYPosition) > defaultViewHeight / 4) {
-                                      //  closeUpAndDismissDialog(currentYPosition);
-                                        popWindow.dismiss();
+                                       closeUpAndDismissDialog(currentYPosition);
+                                       // popWindow.dismiss();
                                         return true;
                                     }
 
@@ -529,8 +504,8 @@ public class MainActivity extends AppCompatActivity implements Search.OndataPass
                                 // Has user scroll enough to "auto close" popup ?
                                 if (Math.abs(baseLayoutPosition - currentYPosition) > defaultViewHeight / 2)
                                 {
-                                   // closeDownAndDismissDialog(currentYPosition);
-                                    popWindow.dismiss();
+                                    closeDownAndDismissDialog(currentYPosition);
+                                    //popWindow.dismiss();
                                     return true;
                                 }
 
