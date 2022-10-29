a4342ab505961286f6572f9302f1d1162a4c3455
s0oraj <hotmailsuraj@gmail.com>
2022-10-29 07:10:01 +0530

    Code changes to add stories feature

--- app/src/main/java/blog/cosmos/home/animus/StoryAddActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/StoryAddActivity.java
@@ -1,11 +1,20 @@
 package blog.cosmos.home.animus;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.gowtham.library.utils.CompressOption;
+import com.gowtham.library.utils.LogMessage;
+import com.gowtham.library.utils.TrimType;
+import com.gowtham.library.utils.TrimVideo;
 
 public class StoryAddActivity extends AppCompatActivity {
 
@@ -32,8 +41,32 @@ public class StoryAddActivity extends AppCompatActivity {
 
             Uri uri = data.getData();
 
+            TrimVideo.activity(String.valueOf(uri))
+                    .setCompressOption(new CompressOption())
+                    .setTrimType(TrimType.MIN_MAX_DURATION)
+                    .setMinToMax(5,30)
+                    .setHideSeekBar(true)
+                    .start(this,startForResult);
         }
 
 
     }
+
+
+
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK &&
+                        result.getData() != null) {
+                    Uri uri = Uri.parse(TrimVideo.getTrimmedVideoPath(result.getData()));
+                  //  Log.d(TAG, "Trimmed path:: " + uri);
+
+                } else{
+                    //  LogMessage.v("videoTrimResultLauncher data is null");
+                }
+
+            });
+
+
 }
