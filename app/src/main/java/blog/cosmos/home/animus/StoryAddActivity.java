35c1d0d372cec6a51de4a54a7e596a95f3641c93
s0oraj <hotmailsuraj@gmail.com>
2022-10-29 06:57:10 +0530

    Code changes to add stories feature

---
+++ app/src/main/java/blog/cosmos/home/animus/StoryAddActivity.java
@@ -0,0 +1,14 @@
+package blog.cosmos.home.animus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_add);
+    }
+}
--- app/src/main/java/blog/cosmos/home/animus/adapter/StoriesAdapter.java
+++ app/src/main/java/blog/cosmos/home/animus/adapter/StoriesAdapter.java
@@ -1,16 +1,23 @@
 package blog.cosmos.home.animus.adapter;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.helper.widget.Layer;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.util.List;
 
@@ -63,6 +70,37 @@ public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.StoriesH
         holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(position == 0){
+
+                    Dexter.withContext(activity)
+                            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            .withListener(new MultiplePermissionsListener() {
+                                @Override
+                                public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                                    if(multiplePermissionsReport.areAllPermissionsGranted()){
+
+                                    } else{
+                                        Toast.makeText(activity, "Please allow permission from settings.", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                                    permissionToken.continuePermissionRequest();
+                                }
+                            }).check();
+
+
+
+
+                } else{
+
+                }
 
             }
         });
