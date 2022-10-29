6cc792203363c7ae5f85bc25eb8a131c12bc1cbe
s0oraj <hotmailsuraj@gmail.com>
2022-10-29 21:03:50 +0530

    Code changes to add stories feature

--- app/src/main/java/blog/cosmos/home/animus/fragments/Profile.java
+++ app/src/main/java/blog/cosmos/home/animus/fragments/Profile.java
@@ -21,6 +21,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -54,15 +55,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.marsad.stylishdialogs.StylishAlertDialog;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -77,6 +82,7 @@ import java.util.Map;
 
 import blog.cosmos.home.animus.R;
 import blog.cosmos.home.animus.ReplacerActivity;
+import blog.cosmos.home.animus.chat.ChatActivity;
 import blog.cosmos.home.animus.model.PostImageModel;
 import de.hdodenhof.circleimageview.CircleImageView;
 import kotlin.jvm.internal.MagicApiIntrinsics;
@@ -92,7 +98,7 @@ public class Profile extends Fragment {
     List<Object> followersList, followingList, followingList_2;
     private TextView nameTv, toolbarNameTv, statusTv, followingCountTv, followersCountTv, postCountTv;
     private CircleImageView profileImage;
-    private Button followBtn;
+    private Button followBtn, startChatBtn;
     private RecyclerView recyclerView;
 
     private LinearLayout countLayout;
@@ -144,6 +150,12 @@ public class Profile extends Fragment {
             editProfileBtn.setVisibility(View.VISIBLE);
             followBtn.setVisibility(View.GONE);
             countLayout.setVisibility(View.VISIBLE);
+
+
+            //Hide chat btn
+            startChatBtn.setVisibility(View.GONE);
+
+
         } else {
             editProfileBtn.setVisibility(View.GONE);
             followBtn.setVisibility(View.VISIBLE);
@@ -196,8 +208,9 @@ public class Profile extends Fragment {
             @Override
             public void onClick(View view) {
                 if (isFollowed) {
-                    followersList.remove(user.getUid());
-                    followingList_2.remove(userUID);
+                    followersList.remove(user.getUid()); // opposite user
+
+                    followingList_2.remove(userUID); /us
 
 
                     final Map<String, Object> map_2 = new HashMap<>();
@@ -230,6 +243,8 @@ public class Profile extends Fragment {
 
 
                 } else {
+
+                    createNotification();
                     followersList.add(user.getUid());
                     followingList_2.add(userUID);
 
@@ -289,6 +304,103 @@ public class Profile extends Fragment {
 
     }
 
+
+    void queryChat() {
+
+        assert getContext() != null;
+        StylishAlertDialog alertDialog = new StylishAlertDialog(getContext(), StylishAlertDialog.PROGRESS);
+        alertDialog.setTitleText("Starting Chat...");
+        alertDialog.setCancellable(false);
+        alertDialog.show();
+
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Messages");
+        reference.whereArrayContains("uid", userUID)
+                .get().addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()) {
+
+                        QuerySnapshot snapshot = task.getResult();
+
+                        if (snapshot.isEmpty()) {
+                            startChat(alertDialog);
+                        } else {
+                            //get chatId and pass
+                            alertDialog.dismissWithAnimation();
+                            for (DocumentSnapshot snapshotChat : snapshot) {
+
+                                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                                intent.putExtra("uid", userUID);
+                                intent.putExtra("id", snapshotChat.getId()); //return doc id
+                                startActivity(intent);
+                            }
+
+
+                        }
+
+                    } else
+                        alertDialog.dismissWithAnimation();
+
+                });
+
+    }
+
+
+    void startChat(StylishAlertDialog alertDialog) {
+
+
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Messages");
+
+        List<String> list = new ArrayList<>();
+
+        list.add(0, user.getUid());
+        list.add(1, userUID);
+
+        String pushID = reference.document().getId();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", pushID);
+        map.put("lastMessage", "Hi");
+        map.put("time", FieldValue.serverTimestamp());
+        map.put("uid", list);
+
+        reference.document(pushID).update(map).addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                reference.document(pushID).set(map);
+            }
+        });
+
+        //todo - ---- - -- - -
+        //Message
+
+        CollectionReference messageRef = FirebaseFirestore.getInstance()
+                .collection("Messages")
+                .document(pushID)
+                .collection("Messages");
+
+        String messageID = messageRef.document().getId();
+
+        Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("id", messageID);
+        messageMap.put("message", "Hi");
+        messageMap.put("senderID", user.getUid());
+        messageMap.put("time", FieldValue.serverTimestamp());
+
+        messageRef.document(messageID).set(messageMap);
+
+        new Handler().postDelayed(() -> {
+
+            alertDialog.dismissWithAnimation();
+
+            Intent intent = new Intent(getActivity(), ChatActivity.class);
+            intent.putExtra("uid", userUID);
+            intent.putExtra("id", pushID);
+            startActivity(intent);
+
+        }, 3000);
+
+    }
+
+
     private void init(View view) {
 
         mContext=getContext();
@@ -309,6 +421,7 @@ public class Profile extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView);
         countLayout = view.findViewById(R.id.countLayout);
         editProfileBtn = view.findViewById(R.id.edit_profileImage);
+        startChatBtn = view.findViewById(R.id.startChatBtn);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
@@ -317,8 +430,8 @@ public class Profile extends Fragment {
 
     }
 
+    @SuppressLint("SetTextI18n")
     private void loadBasicData() {
-
 
         userRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
@@ -335,7 +448,7 @@ public class Profile extends Fragment {
                     String name = value.getString("name");
                     String status = value.getString("status");
 
-                    String profileURL = value.getString("profileImage");
+                    final String profileURL = value.getString("profileImage");
 
                     nameTv.setText(name);
                     toolbarNameTv.setText(name);
@@ -357,6 +470,7 @@ public class Profile extends Fragment {
 
                     try {
 
+                        assert getContext() != null;
                         Glide.with(getContext().getApplicationContext())
                                 .load(profileURL)
                                 .placeholder(R.drawable.ic_person)
@@ -387,9 +501,12 @@ public class Profile extends Fragment {
                         followBtn.setText("Unfollow");
                         isFollowed = true;
 
+                        startChatBtn.setVisibility(View.VISIBLE);
                     } else {
                         followBtn.setText("Follow");
                         isFollowed = false;
+
+                        startChatBtn.setVisibility(View.GONE);
 
 
                     }
@@ -421,8 +538,11 @@ public class Profile extends Fragment {
 
         File directory = contextWrapper.getDir("image_data", Context.MODE_PRIVATE);
 
-        if(!directory.exists())
+        if(!directory.exists()){
             directory.mkdirs();
+            Log.d("Directory", String.valueOf(isMade));
+        }
+
 
         File path = new File(directory, "profile.png");
         FileOutputStream outputStream = null;
@@ -524,7 +644,7 @@ public class Profile extends Fragment {
     }
 
     public void uploadImage(Uri uri) {
-        StorageReference reference = FirebaseStorage.getInstance().getReference().child("Profile Images");
+       final StorageReference reference = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
         reference.putFile(uri)
                 .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
@@ -555,6 +675,7 @@ public class Profile extends Fragment {
                                                             if (task.isSuccessful()) {
                                                                 Toast.makeText(getContext(), "Updated Successfully", Toast.LENGTH_SHORT).show();
                                                             } else {
+                                                                assert task.getException() != null;
                                                                 Toast.makeText(getContext(), "Error: " + task.getException().getMessage(),
                                                                         Toast.LENGTH_SHORT).show();
 
@@ -567,6 +688,7 @@ public class Profile extends Fragment {
                                         }
                                     });
                         } else {
+                            assert task.getException() != null;
                             Toast.makeText(getContext(), "Error: " + task.getException().getMessage(),
                                     Toast.LENGTH_SHORT).show();
 
@@ -577,6 +699,22 @@ public class Profile extends Fragment {
 
     }
 
+
+    void createNotification() {
+
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Notifications");
+
+        String id = reference.document().getId();
+        Map<String, Object> map = new HashMap<>();
+        map.put("time", FieldValue.serverTimestamp());
+        map.put("notification", user.getDisplayName() + " followed you.");
+        map.put("id", id);
+        map.put("uid", userUID);
+
+
+        reference.document(id).set(map);
+
+    }
     private static class PostImageHolder extends RecyclerView.ViewHolder {
 
         private ImageView imageView;
