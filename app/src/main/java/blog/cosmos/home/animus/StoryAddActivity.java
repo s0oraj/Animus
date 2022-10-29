646ccd444190705b96ed20ff9d711f5849cc385b
s0oraj <hotmailsuraj@gmail.com>
2022-10-29 07:53:48 +0530

    Code changes to add stories feature

--- app/src/main/java/blog/cosmos/home/animus/StoryAddActivity.java
+++ app/src/main/java/blog/cosmos/home/animus/StoryAddActivity.java
@@ -2,6 +2,7 @@ package blog.cosmos.home.animus;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,15 +14,29 @@ import android.util.Log;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.gowtham.library.utils.CompressOption;
 import com.gowtham.library.utils.LogMessage;
 import com.gowtham.library.utils.TrimType;
 import com.gowtham.library.utils.TrimVideo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StoryAddActivity extends AppCompatActivity {
 
 
     VideoView videoView;
+    FirebaseUser user;
 
     private static final int SELECT_VIDEO = 101;
 
@@ -34,6 +49,8 @@ public class StoryAddActivity extends AppCompatActivity {
 
                     videoView.setVideoURI(uri);
                     videoView.start();
+
+                    uploadVideoToStorage(uri);
 
                 } else{
                     //  LogMessage.v("videoTrimResultLauncher data is null");
@@ -57,9 +74,57 @@ public class StoryAddActivity extends AppCompatActivity {
 
     }
 
+     void uploadVideoToStorage(Uri uri){
+
+         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Stories");
+         storageReference.putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+             @Override
+             public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                 if(task.isSuccessful()){
+
+                     task.getResult().getStorage().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                         @Override
+                         public void onSuccess(Uri uri) {
+
+
+
+                         }
+                     });
+
+
+                 } else{
+                     String error = task.getException().getMessage();
+                     Toast.makeText(StoryAddActivity.this, "Error: "+error, Toast.LENGTH_SHORT).show();
+                 }
+
+             }
+         });
+     }
+
+     void uploadVideoDataToFirestore(String url){
+         CollectionReference reference = FirebaseFirestore.getInstance().collection("Users").document(user.getUid())
+                 .collection("Stories");
+
+         String id = reference.document().getId();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("videoUrl", url);
+        map.put("id",id);
+        map.put("uid",user.getUid());
+        map.put("name",user.getDisplayName());
+
+        reference.document(id)
+                .set(map);
+
+     }
+
     void init(){
 
     videoView = findViewById(R.id.videoView);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
 
     }
